2ffc8f113232c1aa3a65d53ab204a6540cba2240
Visnu <visnu@eoxys.com>
2020-02-19 15:53:15 +0530

    third
    third commit

--- app/src/main/java/com/eoxys/mybus/controller/PhotoDisplayActivity.java
+++ app/src/main/java/com/eoxys/mybus/controller/PhotoDisplayActivity.java
@@ -2,17 +2,24 @@ package com.eoxys.mybus.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.eoxys.mybus.R;
+
+import java.io.File;
 
 public class PhotoDisplayActivity extends AppCompatActivity {
 
     String img_path;
 
     ImageView imgview;
+    ImageButton shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,8 @@ public class PhotoDisplayActivity extends AppCompatActivity {
 
         imgview = (ImageView) findViewById(R.id.imgdisplay);
 
+        shareButton = (ImageButton) findViewById(R.id.shareButton);
+
         if(getIntent().hasExtra("Img_path")){
 
             img_path = getIntent().getStringExtra("Img_path");
@@ -28,5 +37,15 @@ public class PhotoDisplayActivity extends AppCompatActivity {
             Glide.with(this).load(img_path).into(imgview);
 
         }
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("image/*");
+                share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(img_path)));
+                startActivity(Intent.createChooser(share,"Share via"));
+            }
+        });
     }
 }
